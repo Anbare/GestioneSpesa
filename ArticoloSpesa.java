b279642fc1380f0b1a2999a1b9fa2dd7f1cbe33e
Andrea Barbieri <barbieriforjob@gmail.com>
2026-05-08 10:36:13 +0200

    aggiunti file java

---
+++ ArticoloSpesa.java
@@ -0,0 +1,5 @@
+package com.generation.lessons.gestioneSpesa;
+
+public class ArticoloSpesa {
+
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.generation.lessons.gestioneSpesa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
